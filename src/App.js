a13fb39340912f01c50a09744e5b3892bcf8c5da
omid <o.shali01@gmail.com>
2024-02-18 17:02:24 +0330

    load data from source folder

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
 import React, { useState } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
-import { slides } from "./data/data";
+// import { slides } from "./data/data";
+import img01 from "./img/01.jpg";
+import img02 from "./img/02.jpg";
+import img03 from "./img/03.jpg";
+import img04 from "./img/04.jpg";
+import img05 from "./img/05.jpg";
 
 function App() {
+  const images = [img01, img02, img03, img04, img05];
   const [currentIndex, setCurrentIndex] = useState(0);
   const prevSlide = () => {
     const isFirstSlid = currentIndex === 0;
-    setCurrentIndex(isFirstSlid ? slides.length - 1 : currentIndex - 1);
+    setCurrentIndex(isFirstSlid ? images.length - 1 : currentIndex - 1);
   };
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
+    const isLastSlide = currentIndex === images.length - 1;
     setCurrentIndex(isLastSlide ? 0 : currentIndex + 1);
   };
   return (
-    <div className="flex flex-col w-full h-screen m-auto justify-center bg-gradient-to-t from-[#5ee7df] to-[#b490ca] p-4 pb-16">
-      <div className="relative max-w-[1640px] w-full h-full shadow-xl">
+    <div className="flex flex-col w-full h-screen m-auto justify-center items-center bg-gradient-to-t from-[#5ee7df] to-[#b490ca] p-4 pb-16">
+      <div className="relative max-w-[1640px] w-full h-full shadow-xl ">
         <div
-          className="bg-cover bg-center duration-500 z-50 w-full h-full rounded-xl "
+          className="bg-cover bg-center duration-1000 w-full h-full rounded-xl "
           style={{
-            backgroundImage: `url(${slides[currentIndex].url})`,
+            backgroundImage: `url(${images[currentIndex]})`,
           }}
         />
         {/* Left slider bytton */}
         <div
           onClick={prevSlide}
-          className="text-white absolute top-[50%] left-4 -translate-x-0 translate-y-[-50%] cursor-pointer bg-black/50 p-2 rounded-full text-2xl"
+          className="text-white absolute top-[50%] left-4 -translate-x-0 translate-y-[-50%] cursor-pointer bg-black/50 p-2 rounded-full"
         >
-          <BsChevronCompactLeft size={20} className="" />
+          <BsChevronCompactLeft size={20} />
         </div>
         {/* Right slider bytton */}
         <div
           onClick={nextSlide}
-          className="text-white absolute top-[50%] right-4 -translate-x-0 translate-y-[-50%] cursor-pointer bg-black/50 p-2 rounded-full text-2xl"
+          className="text-white absolute top-[50%] right-4 -translate-x-0 translate-y-[-50%] cursor-pointer bg-black/50 p-2 rounded-full"
         >
-          <BsChevronCompactRight size={20} className="" />
+          <BsChevronCompactRight size={20} />
         </div>
       </div>
       <div className="w-full flex justify-center gap-2 mt-4">
-        {slides.map((slid, slideindex) => (
+        {images.map((slid, slideindex) => (
           <div
             className={`cursor-pointer text-gray-600/50  hover:scale-150 hover:text-gray-600/70 ${
               currentIndex === slideindex ? "scale-125 text-gray-700/70" : ""
@@ -48,7 +54,6 @@ function App() {
           >
             <RxDotFilled size={25} />
           </div>
-          // cursor-pointer text-gray-600/50 active:text-gray-800 hover:scale-150 hover:text-gray-600/70
         ))}
       </div>
     </div>
--- src/data/data.js
+++ src/data/data.js
@@ -16,3 +16,7 @@ export const slides = [
     url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
   },
 ];
+
+export const images =[
+  {img :""}
+]
